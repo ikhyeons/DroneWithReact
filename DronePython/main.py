bb40bc3935602d01c3c2b33b6ed3786650aeb5dd
ikhyeons <skantrkwl789@naver.com>
2022-10-07 00:17:58 +0900

    파일 위치 변경

--- DronePython/main.py
+++ DronePython/main.py
@@ -1,53 +1,59 @@
-import threading
-import socket
-import platform
-import sys
-import time
+#캡디 / 권재호, 성익현, 김혜원
+import threading #메인 루프 외에 다른 루프를 동시에 돌려야함 / 이 경우 수신 루프
+import socket #UDP 통신을 위한 라이브러리
+
+
+print('\r\n\r\n파일 시작 됨.\r\n')
 
 host = ''
 port = 9000
 locaddr = (host, port)
-
-print(socket.AF_INET)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-tello_address = ('192.168.10.1', 8889) #8889 포트
-sock.bind(locaddr) #9000포트
+tello_address = ('192.168.10.1', 8889)  #텔로 8889 포트
+server_address = ('127.0.0.1', 3001) #웹 백 포트
+sock.bind(locaddr) #파이썬 소켓 9000포트
 
-def recv():
+#나중에 웹 연결이 끝나면 메인 루프로 변경할 것.
+#입력을 웹에서만 받을 것이기 때문에 리시브 쓰레드를 유지할 필요가 없음.
+
+def recv():  # 데이터 받기
     count = 0
     while True:
         try:
-            data, server = sock.recvfrom(1518)
-            print(data.decode(encoding="utf-8"))
+            data, server = sock.recvfrom(2024);
+            print(data.decode(encoding="utf-8"), server, 'this')
+            #데이터를 받은 후 다시 내보냄
+            sent2 = sock.sendto(data, server_address);
         except Exception:
-            print('\nExit . . .\n')
+            print('\nExit . . . . \n')
             break
 
-print('\r\n\r\nTello Python3 Demo.\r\n')
-print('Tello: command takeoff land flip forward back left right \r\n       up down cw ccw speed speed?\r\n')
-print('end -- quit demo.\r\n')
-
-# recvThread create
+# 리시브 쓰레드 생성=
 recvThread = threading.Thread(target=recv)
 recvThread.start()
 
-while True:
+while True: #데이터 보내기
     try:
-        msg = input("");
+        msg = input();
+        print(msg);
 
         if not msg:
             break
 
-        if 'end' in msg:
+        if 'End' in msg:
             print('...')
             sock.close()
             break
 
-        # Send data
+        # 데이터 송신
         msg = msg.encode(encoding="utf-8")
-        sent = sock.sendto(msg, tello_address)
+        sent1 = sock.sendto(msg, tello_address);
+
     except KeyboardInterrupt:
         print('\n . . .\n')
         sock.close()
         break
+
+
+
