5098d8a325e4997b4ac1dcb5b718aacd3198b25d
ikhyeons <skantrkwl789@naver.com>
2022-10-07 00:04:35 +0900

    .

--- DroneWeb/client/server/server.js
+++ DroneWeb/client/server/server.js
@@ -1,40 +1,37 @@
-//--------------------------------------------------------------------------라이브러리, 기본 설정
+//--------------------------------------------------------------라이브러리, 기본 설정
 //웹 익스프레스
-const express = require('express')
+const express = require("express");
 const port = 3001;
-const app = express()
+const app = express();
 //UDP통신 datagram
-const dgram = require('dgram')
-const server = dgram.createSocket('udp4');
-//---------------------------------------------------------------------------웹 코드
+const dgram = require("dgram");
+const server = dgram.createSocket("udp4");
+//---------------------------------------------------------------웹 코드
 //백 포트 개방
-app.listen(port, ()=>{
-    console.log(`port started with ${port}`)
-})
+app.listen(port, () => {
+  console.log(`port started with ${port}`);
+});
 //인덱스 페이지 라우팅
-app.get('/',(req,res)=>{
-    res.send('ㅎㅇ2')
-})
-//----------------------------------------------------------------------------파이썬 연결 코드
-  //클라이언트로부터 메시지 수신 시
-  server.on('message', (msg, remote_info) => {
-    console.log(`server got: ${msg} from ${remote_info.address}:${remote_info.port}`);
-  });
+app.get("/", (req, res) => {
+  res.send("ㅎㅇ2");
+});
+//----------------------------------------------------------------파이썬 연결 코드
+//클라이언트로부터 메시지 수신 시
+server.on("message", (msg, remote_info) => {
+  console.log(
+    `server got: ${msg} from ${remote_info.address}:${remote_info.port}`
+  );
+});
 
-  server.on('connection', () => {
-    console.log('someone Connected');
-  });
+server.on("connection", () => {
+  console.log("someone Connected");
+});
 
-  server.bind(port, 'localhost');
+server.bind(port, "localhost");
 
-  //서버 start시
-  server.on('listening', () => {
-    const address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
-    server.send('messege', 9000, 'localhost', ()=>{
-      console.log('send this')
-    })
-  });
-
-  
-
+//서버 start시
+server.on("listening", () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+  server.send("node server is running...", 9000, "127.0.0.1", () => {});
+});
